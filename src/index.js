bd8a262cfbf51f13371c06bb38929af055ce7367
Marnie Robinson <marnielane@gmail.com>
2021-03-16 20:52:48 -0600

    refactor

--- src/index.js
+++ src/index.js
@@ -1,83 +1,103 @@
-const baseURL = `http://localhost:3000/colors`
+const baseURL = `http://localhost:3000/colors/`
 const cardContainer = document.querySelector('#card-container')
 const addColorForm = document.getElementById('add-color-form')
 
 fetch(baseURL)
     .then(parseJson)
     .then(interateOverColors)
+addNewColorFormListener()
 
 function interateOverColors(colors) {
     colors.forEach(renderColors)
 }
 
 function renderColors(color) {
-        const colorCard = document.createElement('div')
-        colorCard.className = 'color-card'
-        colorCard.style.backgroundColor = `${color.hex}`
+    const colorCard = document.createElement('div')
+    colorCard.className = 'color-card'
+    colorCard.style.backgroundColor = `${color.hex}`
 
-        const colorName = document.createElement('h2')
-        colorName.innerText = color.name
+    const colorName = document.createElement('h2')
+    colorName.innerText = color.name
 
-        const voteCounter = document.createElement('p')
-        voteCounter.textContent = `${color.votes} Votes`
+    const voteCounter = document.createElement('p')
+    voteCounter.innerText = `${color.votes} Votes`
 
-        const voteButton = document.createElement('button')
-        voteButton.innerText = '+1 Vote'
-        voteButton.addEventListener('click',(e) => {
-            color.votes++
-            fetch(`${baseURL}/${color.id}`,{
-                method:'PATCH',
-                headers:{
-                    "Content-Type":"application/JSON"
-                },
-                body:JSON.stringify({
-                    votes: color.votes
-                }),
-            })
-            console.log('response.json')
-            voteCounter.textContent = `${color.votes} Votes`
-        })
+    const voteButton = document.createElement('button')
+    voteButton.textContent = '+1 Vote'
+    voteButton.addEventListener('click', (e) => {
+        voteForColor(color, voteCounter)
+    })
 
-        const deleteButton = document.createElement('button')
-        deleteButton.className = 'delete-button'
-        deleteButton.innerHTML = '&times;'
+    const deleteButton = document.createElement('button')
+    deleteButton.innerHTML = '&times;'
 
-        deleteButton.addEventListener('click',()=>{
-            colorCard.remove()
-            fetch(`${baseURL}/${color.id}`, {
-                method:"DELETE",
-            })
-        })
-        
+    deleteButton.addEventListener('click', () => {
+        colorCard.remove()
+        deleteColor(color.id)
+    })
 
-        colorCard.append(colorName, voteCounter, voteButton, deleteButton)
-        cardContainer.append(colorCard)
-        
- }
+    colorCard.append(colorName, voteCounter, voteButton, deleteButton)
+    cardContainer.append(colorCard)
+}
 
-function addColor(){
-    addColorForm.addEventListener('submit',(e) =>{
-        e.preventDefault()
-        const addColorFormData = new FormData(e.target)
-        const colorName = addColorFormData.get('name')
-        const colorHex = addColorFormData.get('hex')
-        fetch(`${baseURL}`,{
-            method:"POST",
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify({
-                name: colorName,
-                hex: colorHex,
-                votes: 0
-            }),
-        }).then(window.location.reload())
+function deleteColor(id) {
+    fetch(baseURL + id, {
+        method: "DELETE",
     })
 }
 
+function voteForColor(color, voteCounter) {
+    color.votes++
+
+    let updatedColor = {
+        votes: color.votes,
+    }
+
+    let options = {
+        method: 'PATCH',
+        headers: {
+            "Content-Type": "application/JSON",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(updatedColor),
+    }
+
+    fetch(baseURL + color.id, options)
+        .then(parseJson)
+        .then(
+            (returnUpdatedColor) =>
+                (voteCounter.innerText = `Votes ${returnUpdatedColor.votes}`)
+        )
+}
+
+function addNewColorFormListener() {
+    addColorForm.addEventListener('submit', (e) => {
+        let colorForm = e.target
+        e.preventDefault()
+
+        let color = {
+            name: colorForm.name.value,
+            hex: colorForm.hex.value,
+            votes: 0,
+        }
+
+        let options = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: JSON.stringify(color),
+        }
+
+        fetch(baseURL, options)
+            .then(parseJson)
+            .then((newColor) => renderColors(newColor))
+        // .then(window.location.reload())
+    })
+}
 
 function parseJson(response) {
     return response.json()
 }
 
-addColor()
